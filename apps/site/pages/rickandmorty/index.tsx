a8c225c524b5198f71c76deff9e45c076a3382ea
Vu Nguyen <shardphoneix@gmail.com>
2022-05-30 11:28:12 -0700

    add content for landing page to define what the starter is using
    Signed-off-by: Vu Nguyen <shardphoneix@gmail.com>

--- apps/site/pages/rickandmorty/index.tsx
+++ apps/site/pages/rickandmorty/index.tsx
@@ -1,10 +1,22 @@
 import Link from 'next/link';
+import { Anchor, Center, List, ListItem, Text } from '@mantine/core';
 
 export default function RickandmortyIndexPage() {
   return (
-    <div>
-      <h1>Welcome to Rickandmorty!</h1>
-      <Link href="/rickandmorty/characters">Characters</Link>
-    </div>
+    <>
+      <Center>
+        <Text weight={700}>
+          An example of integrating with a{' '}
+          <Anchor href="https://rickandmortyapi.com/">3rd party api</Anchor>
+        </Text>
+      </Center>
+      <List>
+        <ListItem>
+          <Link href="/rickandmorty/characters" passHref>
+            <Anchor>Characters</Anchor>
+          </Link>
+        </ListItem>
+      </List>
+    </>
   );
 }
